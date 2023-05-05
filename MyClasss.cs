cd2f3b4658d2689408873ec687297c01a10ddf07
Antonymos777 <118302598+Antonymos777@users.noreply.github.com>
2023-05-05 22:15:33 +0300

    Add files via upload

---
+++ MyClasss.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace StackFrameMy
+{
+    public static class StackFrameExtensions
+    {
+      public static Dictionary<string, object> GetLocalVariables(this StackFrame frame) // получает все локальные переменные из метода, связанного с переданным объектом StackFrame,
+                                                                                        // и возвращает словарь, где ключами являются индексы локальных переменных, а значениями - их типы.
+        {
+    var localVariables = new Dictionary<string, object>();
+    var method = frame.GetMethod();
+    var locals = method.GetMethodBody()?.LocalVariables;
+
+    if (locals != null)
+    {
+        foreach (var local in locals)
+        {
+            localVariables[local.LocalIndex.ToString()] = local.LocalType;
+        }
+    }
+
+    return localVariables;
+}
+
+
+
+        public static Dictionary<string, object> GetLocalVariables<T>(this StackFrame frame) // работает так же, но только
+                                                                                             //возвращает только те локальные переменные, которые имеют тип T.
+        {
+            var localVariables = new Dictionary<string, object>();
+            var method = frame.GetMethod();
+            var locals = method.GetMethodBody()?.LocalVariables;
+
+            if (locals != null)
+            {
+                foreach (var local in locals)
+                {
+                    if (local.LocalType == typeof(T))
+                    {
+                        localVariables[local.LocalIndex.ToString()] = frame.GetMethod().Module.ResolveSignature(local.LocalType.MetadataToken);
+                    }
+                }
+            }
+
+            return localVariables;
+        }
+    }
+    public static class MyClasss // содержит метод GetCurrentStackFrame(), который возвращает верхний элемент стека вызовов, используя класс StackTrace.
+    {
+       
+        public static StackFrame GetCurrentStackFrame() // возвращает верхний элемент стека вызовов, используя класс StackTrace.
+        {
+
+            StackTrace trace = new StackTrace(); //создаю класс StackTrace 
+            //StackTrace trac = new StackTrace(1);  //предоставляет методы и свойства, которые позволяют получить информацию о каждом элементе стека вызовов
+            StackFrame frame =trace.GetFrame(0); //верхний элемент стека
+
+            return frame;
+
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackFrameMy
+{
+    public class Program
+    {
+        static void Main()
+        {
+            StackFrame frame = MyClasss.GetCurrentStackFrame();
+            var localVariables = frame.GetLocalVariables();
+            foreach (var variable in localVariables)
+            {
+                Console.WriteLine($"{variable.Key} - {variable.Value}");
+            }
+        /*StackFrame fram = MyClasss.GetCurrentStackFrame();
+            Console.WriteLine("Метод:" + frame.GetMethod().Name);
+            Console.WriteLine("Строка: " + frame.GetFileLineNumber());
+        //    Console.WriteLine("Метод:" + fram.GetMethod().Name);
+       //  Console.WriteLine("Строка: " + fram.GetFileLineNumber()); */
+            Console.ReadKey();
+        }
+
+      
+    }
+}
